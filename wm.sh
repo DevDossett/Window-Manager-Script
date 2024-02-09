0d565093bb2fa3f03302f6daf49438591849497d
DevDossett <github@dossett.dev>
2024-02-08 21:13:37 -0600

    Cleaned up comments

--- wm.sh
+++ wm.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
 
-# Find the window ID of Visual Studio Code
-vscode_window_id=$(wmctrl -l | grep "Visual Studio Code" | awk '{print $1}')
-
-# Check if the window ID was found
-# if [ ! -z "$vscode_window_id" ]; then
-#     wmctrl -i -r "$vscode_window_id" -e 0,18,1132,900,1330
-# else
-#     echo "Visual Studio Code not open."
-# fi
-
-# brave_window_id=$(wmctrl -l | grep "Brave" | awk '{print $1}')
-
-# if [ ! -z "$brave_window_id" ]; then
-#     wmctrl -i -r "$brave_window_id" -e 0,920,1122,1862,1410
-# else
-#     echo "Brave not open."
-# fi
-
-# dev_tools_window_id=$(wmctrl -l | grep "DevTools" | awk '{print $1}')
-
-# if [ ! -z "$dev_tools_window_id" ]; then
-#     wmctrl -i -r "$dev_tools_window_id" -e 0,2784,1132,642,1330
-# else
-#     echo "DevTools not open."
-# fi
-
-
+## Add windows here. The format is Window Name|wmctrl coords
+# Run wmctrl -lG to list all currently open windows
 windows=("Visual Studio Code|0,18,1132,900,1330" "Brave|0,920,1122,1862,1410" "DevTools|0,2784,1132,642,1330")
 
+# Loop through windows array
 for window_info in "${windows[@]}"; do
+    # Get the window name from the first column
     name=$(echo "$window_info" | awk -F '|' '{print $1}')
+    # Getting the window id by using grep to get the line
+    # the name is on, then using awk to grab the id from the
+    # first column
     id=$(wmctrl -l | grep "$name" | awk '{print $1}')
+    # Getting the cords from the windows awway
     coords=$(echo "$window_info" | awk -F '|' '{print $2}')
+    # If the id exists then run the wmctrl command
     if [ ! -z "$id" ]; then
         wmctrl -i -r "$id" -e $coords
     else
+        # if not print window not open
         echo "$name not open."
     fi
 done
